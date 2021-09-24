9918af14133937c5f87eee18810ec36c824c6681
pucahuachi <pucahuachi@gmail.com>
2021-09-24 10:19:24 -0300

    Inclusao do GRASP

---
+++ src/GRASP.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Puca Penna on 03/10/19.
+//
+
+#include <cmath>
+#include <iostream>
+#include <ctime>
+#include "GRASP.h"
+#include "Construcao.h"
+#include "Util.h"
+#include "Descida.h"
+#include "Arquivos.h"
+
+float GRASP(int n, std::vector<int> &s, float **d, float alpha, int GRASP_max /*, int tipo_construcao*/)
+{
+    vector<int> s_star;
+    int iter = 0;
+    float fo_star = INT_MAX;
+    float fo;
+
+
+    
+    s = s_star;
+
+    return fo_star;
+}
+
---
+++ src/GRASP.h
@@ -0,0 +1,15 @@
+//
+// Created by Puca Penna on 03/10/19.
+//
+
+#ifndef _PCV_GRASP_H_
+#define _PCV_GRASP_H_
+
+#include <vector>
+
+float GRASP(int n, std::vector<int> &s, float **d, float alpha, int GRASP_max /*, int tipo_construcao*/);
+
+/* GRASP REATIVO */
+//float R_GRASP(int n, std::vector<int> &s, float **d, float alpha[], int GRASP_max /*, int tipo_construcao*/);
+
+#endif //_PCV_C_GRASP_H_
