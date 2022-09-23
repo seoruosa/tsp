bd75befa90304330e270e4decf901fcac8caae3f
seoruosa <thiago.giachetto@gmail.com>
2022-09-23 09:41:16 -0300

    update to use max value from numeric library

--- src/GRASP.cpp
+++ src/GRASP.cpp
@@ -10,12 +10,13 @@
 #include "Util.h"
 #include "Descida.h"
 #include "Arquivos.h"
+#include <limits>
 
 float GRASP(int n, std::vector<int> &s, float **d, float alpha, int GRASP_max /*, int tipo_construcao*/)
 {
     vector<int> s_star;
     int iter = 0;
-    float fo_star = INT_MAX;
+    float fo_star = std::numeric_limits<float>::max();
     float fo;
 
 
